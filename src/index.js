781fed0222b063e477acbe137fa8a23d261a64a3
Artem Bilodid <billodid.sumy@gmail.com>
2021-08-08 15:28:57 +0300

    added templates and main theme logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-import './sass/main.scss';
+import menuItems from './menu.json';
+import menuTemplate from './templates/menu.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const THEME_KEY = 'Theme';
+
+const menuListEl = document.querySelector('.js-menu');
+const themeToggleEl = document.getElementById('theme-switch-toggle');
+const bodyEl = document.querySelector('body');
+
+menuListEl.innerHTML = menuTemplate(menuItems);
+
+const toggleThemeClass = () => {
+  const bodyClassList = bodyEl.classList;
+  if (bodyClassList.contains(Theme.LIGHT)) {
+    bodyClassList.replace(Theme.LIGHT, Theme.DARK);
+    return;
+  }
+  bodyClassList.replace(Theme.DARK, Theme.LIGHT);
+};
+
+const onToggleChange = evt => {
+  const isDarkTheme = evt.target.checked;
+  const themeValue = isDarkTheme ? Theme.DARK : Theme.LIGHT;
+
+  localStorage.setItem(THEME_KEY, themeValue);
+  toggleThemeClass();
+};
+
+themeToggleEl.addEventListener('change', onToggleChange);
+
+const setPreviousThemeOrDefault = () => {
+  try {
+    const prevThemeValue = localStorage.getItem(THEME_KEY);
+    bodyEl.classList.add(prevThemeValue);
+
+    if (prevThemeValue === Theme.DARK) {
+      themeToggleEl.checked = true;
+    }
+  } catch {
+    bodyEl.classList.add(Theme.LIGHT);
+  }
+};
+
+setPreviousThemeOrDefault();
